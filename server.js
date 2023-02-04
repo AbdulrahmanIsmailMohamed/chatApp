d60f56abc5b00579d6c323b319dc4b039854f8b4
Abdulrahman Ismail <abdulrahman.ismail.mohammed@gmail.com>
2023-02-05 01:58:13 +0200

    handling a chat message submit in front , backend

--- server.js
+++ server.js
@@ -10,9 +10,15 @@ app.use(express.static("public"))
 
 io.on("connection", socket => {
     socket.emit("message", "Welcom to chat card !!");
+    // broadcast when a user connected
     socket.broadcast.emit("message", "the user has joined the chat");
+    // run when client disconnect
     socket.on("disconnect", () => {
-        io.emit("message","A user has left the chat")
+        io.emit("message", "A user has left the chat")
+    })
+    // listen for a new message
+    socket.on("chatMessage", (msg) => {
+        io.emit("message", msg)
     })
 })
 
